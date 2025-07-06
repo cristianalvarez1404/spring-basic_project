004d290ae4b68237768ff509522a6280d023ad32
CristianAlvarez <cristianandres1496@gmail.com>
2025-07-06 18:11:04 -0500

    adding JPA and H2

--- src/main/java/com/telusko/simpleWebApp/model/Product.java
+++ src/main/java/com/telusko/simpleWebApp/model/Product.java
@@ -1,13 +1,18 @@
 package com.telusko.simpleWebApp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.*;
+import org.hibernate.annotations.CompositeType;
 import org.springframework.stereotype.Component;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Component
+@Entity
 public class Product {
+    @Id
     private Integer prodId;
     private String prodName;
     private Integer price;
--- src/main/java/com/telusko/simpleWebApp/service/ProductService.java
+++ src/main/java/com/telusko/simpleWebApp/service/ProductService.java
@@ -1,6 +1,8 @@
 package com.telusko.simpleWebApp.service;
 
 import com.telusko.simpleWebApp.model.Product;
+import com.telusko.simpleWebApp.repository.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,55 +11,59 @@ import java.util.List;
 
 @Service
 public class ProductService {
-    List<Product> products = new ArrayList<>(Arrays.asList(
-            new Product(101,"Iphone",5000),
-            new Product(102,"Canon Camera",70000),
-            new Product(103,"Shure Mic",4000)
-    ));
+
+    @Autowired
+    ProductRepo repo;
+
+//    List<Product> products = new ArrayList<>(Arrays.asList(
+//            new Product(101,"Iphone",5000),
+//            new Product(102,"Canon Camera",70000),
+//            new Product(103,"Shure Mic",4000)
+//    ));
 
     public List<Product> getProducts(){
-        return products;
+        return repo.findAll();
     }
 
     public Product getProductById(int productId){
-        Product product = products.stream()
-                .filter(prod -> prod.getProdId() == productId)
-                .findFirst()
-                .orElse(null);
-        return product;
+//        Product product = products.stream()
+//                .filter(prod -> prod.getProdId() == productId)
+//                .findFirst()
+//                .orElse(null);
+//        return product;
+        return repo.findById(productId).orElse(null);
     }
 
     public Product addProduct(Product product){
-        products.add(product);
-        return product;
+//        products.add(product);
+        return repo.save(product);
     }
 
     public Product updateProduct(Product product, Integer id){
-        Product searchProduct = products.stream()
-                .filter(prod -> prod.getProdId() == id)
-                .findFirst()
-                .orElse(null);
-
-        if(!product.getProdName().isEmpty()){
-            searchProduct.setProdName(product.getProdName());
-        }
-        if( product.getPrice() != null){
-            searchProduct.setPrice(product.getPrice());
-        }
-
-        return searchProduct;
+//        Product searchProduct = products.stream()
+//                .filter(prod -> prod.getProdId() == id)
+//                .findFirst()
+//                .orElse(null);
+//
+//        if(!product.getProdName().isEmpty()){
+//            searchProduct.setProdName(product.getProdName());
+//        }
+//        if( product.getPrice() != null){
+//            searchProduct.setPrice(product.getPrice());
+//        }
+        return repo.save(product);
     }
 
     public String deleteProduct(Integer id){
-        int index = 0;
-        for(int i = 0; i < products.size();i++){
-            if(products.get(i).getProdId() == id){
-                index = i;
-                break;
-            }
-        }
-        products.remove(index);
-
+//        int index = 0;
+//        for(int i = 0; i < products.size();i++){
+//            if(products.get(i).getProdId() == id){
+//                index = i;
+//                break;
+//            }
+//        }
+//        products.remove(index);
+        repo.deleteById(id);
         return "Product deleted";
     }
 }
